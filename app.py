feb25f1d3eb6fdb2c9106ae0001e53880e571e01
tuankasfee <59762979+tuankasfee@users.noreply.github.com>
2023-04-26 16:12:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -102,6 +102,7 @@ response = AgGrid(
     update_mode=GridUpdateMode.MODEL_CHANGED,
     data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
     fit_columns_on_grid_load=False,
+    columns_auto_size_mode=1
 )
 
 df = pd.DataFrame(response["selected_rows"])
